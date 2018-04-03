f1b017618e1e8bb4e70f5f69c6081ec075957f59
lastnamearya <arya.jigyasu6815@gmail.com>
2018-04-03 15:50:58 +0530

    devDependencies added in package.json

---
+++ app/index.js
@@ -0,0 +1,24 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// Webpack thing ~ add css to our file when everything bundles
+require('./index.css');
+
+// state
+// lifecycle event
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
