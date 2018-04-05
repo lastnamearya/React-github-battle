ea6e85128164eacf543de37eb0f7cab6b6b2c923
lastnamearya <arya.jigyasu6815@gmail.com>
2018-04-05 13:01:00 +0530

    Users component with PropTypes

--- app/index.js
+++ app/index.js
@@ -2,6 +2,51 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var PropTypes = require('prop-types');
 
+class Users extends React.Component {
+  render() {
+    var friends = this.props.list.filter(function(user) {
+      return user.friend === true;
+    });
+
+    var nonFriends = this.props.list.filter(function(user) {
+      return user.friend !== true;
+    });
+
+    return (
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function(user) {
+            return (
+              <li key={user.name}>
+                {user.name}
+              </li>
+            )
+          })}
+        </ul>
+
+        <hr />
+
+        <h1>Non Friends</h1>
+        <ul>
+          {nonFriends.map(function (user) {
+            return (
+              <li key={user.name}>
+                {user.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+// We can be more specific with PropTypes
+Users.propTypes = {
+  list: PropTypes.array.isRequired
+}
+
 class Badge extends React.Component {
   render() {
     return (
@@ -24,11 +69,24 @@ Badge.propTypes = {
   username: PropTypes.string.isRequired
 }
 
+// ReactDOM.render(
+//   <Badge 
+//     name="Jigyasu Arya"
+//     username="lastnamearya"
+//     img={"https://avatars3.githubusercontent.com/u/6664187?s=400&u=e56c1cfc515eb5e093bb9b0437765c5af86850a8&v=4"}
+//   />,
+//   document.getElementById('app')
+// );
+
 ReactDOM.render(
-  <Badge 
-    name="Jigyasu Arya"
-    username="lastnamearya"
-    img={"https://avatars3.githubusercontent.com/u/6664187?s=400&u=e56c1cfc515eb5e093bb9b0437765c5af86850a8&v=4"}
+  <Users list = {[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false }
+  ]}
   />,
   document.getElementById('app')
 );
