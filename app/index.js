68ebb336b4060a4dcfd563b15ca8d9ca9fb440f4
lastnamearya <arya.jigyasu6815@gmail.com>
2018-04-05 12:00:52 +0530

    Badge Component for our app

--- app/index.js
+++ app/index.js
@@ -1,24 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-// Webpack thing ~ add css to our file when everything bundles
-require('./index.css');
-
-// state
-// lifecycle event
-// UI
-
-class App extends React.Component {
+class Badge extends React.Component {
   render() {
     return (
       <div>
-        Hello Webpack!
+        <img
+          src={this.props.img}
+          alt='Avatar'
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
       </div>
     )
   }
 }
 
 ReactDOM.render(
-  <App />,
+  <Badge 
+    name="Jigyasu Arya"
+    username="lastnamearya"
+    img="https://avatars3.githubusercontent.com/u/6664187?s=400&u=e56c1cfc515eb5e093bb9b0437765c5af86850a8&v=4"
+  />,
   document.getElementById('app')
 );
