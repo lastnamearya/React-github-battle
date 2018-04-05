c9caf6b9b41aecf317bd51f48bb4a7066ec8829d
lastnamearya <arya.jigyasu6815@gmail.com>
2018-04-05 17:40:29 +0530

    this keyword bind ~ to this Component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -6,6 +6,16 @@ class Popular extends React.Component {
     this.state = {
       selectedLanguage: 'All'
     };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function() {
+      return {
+        selectedLanguage: lang
+      }
+    });
   }
 
   render() {
