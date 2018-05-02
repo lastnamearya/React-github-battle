9b4c80661b7afb12807ebb30f399d04c60590dee
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-02 19:50:04 +0530

    rendering profile user Badge

--- app/index.js
+++ app/index.js
@@ -1,9 +1,27 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-require('./index.css');
-var App = require('./components/App');
+
+class Badge extends React.Component {
+  render(){
+    return (
+      <div>
+        <img 
+          src={this.props.img}
+          alt="Avatar"
+          style={{width: 100, height: 100}}
+        />
+        <h1>Name: {this.props.name}</h1>
+        <h3>username: {this.props.username}</h3>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <App />,
+  <Badge 
+    name="Jigyasu Arya"
+    username="lastnamearya"
+    img="https://avatars1.githubusercontent.com/u/6664187?s=460&v=4"
+  />,
   document.getElementById('app')
 );
