3495adb958713da5caac8c07debcf794657fe03e
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-02 20:24:07 +0530

    Users Component with PropTypes added

--- app/index.js
+++ app/index.js
@@ -24,11 +24,67 @@ Badge.propTypes = {
   username: PropTypes.string.isRequired
 }
 
+class Users extends React.Component {
+  render(){
+    var friends = this.props.list.filter(function(user) {
+      return user.friend === true;
+    });
+
+    var nonfriends = this.props.list.filter(function(user) {
+      return user.friend !== true;
+    });
+
+    return(
+      <div>
+        <h1>Friends</h1>
+        <ul>
+          {friends.map(function(user) {
+            return (
+              <li key={user.name}>
+                {user.name}
+              </li>
+            )
+          })}
+        </ul>
+
+        <hr />
+
+        <h1>Non Friends</h1>
+        <ul>
+          {nonfriends.map(function(user){
+            return (
+              <li key={user.name}>
+                {user.name}
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+Users.propTypes = {
+  list: PropTypes.array.isRequired,
+}
+
+// ReactDOM.render(
+//   <Badge 
+//     name="Jigyasu Arya"
+//     username="lastnamearya"
+//     img="https://avatars1.githubusercontent.com/u/6664187?s=460&v=4"
+//   />,
+//   document.getElementById('app')
+// );
+
 ReactDOM.render(
-  <Badge 
-    name="Jigyasu Arya"
-    username="lastnamearya"
-    img="https://avatars1.githubusercontent.com/u/6664187?s=460&v=4"
+  <Users list={[
+    { name: 'Tyler', friend: true },
+    { name: 'Ryan', friend: true },
+    { name: 'Michael', friend: false },
+    { name: 'Mikenzi', friend: false },
+    { name: 'Jessica', friend: true },
+    { name: 'Dan', friend: false } ]} 
   />,
   document.getElementById('app')
 );
