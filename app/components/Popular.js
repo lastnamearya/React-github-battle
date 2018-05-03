98befe6f3e05b9a616b2d280fcec6cf6495242c1
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-03 09:06:23 +0530

    Second argument of map ~ this keyword bind to context

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,9 +1,40 @@
 var React = require('react');
 
 class Popular extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
+    };
+
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  updateLanguage(lang) {
+    this.setState(function(){
+      return {
+        selectedLanguage: lang
+      }
+    });
+  }
+
   render(){
+    var languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+    console.log('Up Here!', this)
+
     return(
-      <div>Popular!</div>
+      <ul className="languages">
+        {languages.map(function(lang) {
+          console.log('Down here!', this)
+          return (
+          <li 
+            onClick={this.updateLanguage}
+            key={lang}>
+              {lang}
+          </li>
+          )
+        }, this)}
+      </ul>
     )
   }
 }
