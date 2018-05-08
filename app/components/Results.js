d8ef639ba5b4f8d0d449a0280ecf3f2be6e3aa4a
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-08 10:12:28 +0530

    queryString package to parse search queries

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,8 +1,12 @@
 var React = require('react');
+// Package to parse query string for us
+var queryString = require('query-string');
 
 class Results extends React.Component {
   render() {
     console.log(this.props);
+    var players = queryString.parse(this.props.location.search);
+    console.log(players);
     return (
       <div>Results</div>
     )
