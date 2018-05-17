5d8f4a35676ffd45801fa6db9e127aa3c80190de
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-17 08:05:24 +0530

    <Loading /> Component

---
+++ app/components/loading.js
@@ -0,0 +1,28 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+var styles = {
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+};
+
+class Loading extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      text: props.text
+    };
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    )
+  }
+}
+
+module.exports = Loading;
