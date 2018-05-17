6d0de588ba226050e55926181d2171dbba98f1ef
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-17 12:46:29 +0530

    bug fixed UglifyJsPlugin now removed in webpack4

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
-// path ~ basically bunch of utilities to work with, comes with nodes
 var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var webpack = require('webpack');
-
 
 var config = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index_bundle.js',
-    // It'll add base to our app where are all components can navigate to
     publicPath: '/'
   },
   module: {
@@ -18,7 +15,6 @@ var config = {
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
   },
-  // Now our app instead of requesting assests from server, just redirect to the target component then React Router will see that and load the Route for e.g /popular
   devServer: {
     historyApiFallback: true,
   },
@@ -26,18 +22,8 @@ var config = {
     new HtmlWebpackPlugin({
       template: 'app/index.html'
     })
-  ]
+  ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development'
 };
-
-if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
-}
 
 module.exports = config;
