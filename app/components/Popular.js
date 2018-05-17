d801e8fd6689cc5c743efc553ebf12386a9a767b
lastnamearya <arya.jigyasu6815@gmail.com>
2018-05-17 09:36:40 +0530

    Loading animation...

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -101,7 +101,7 @@ class Popular extends React.Component{
           selectedLanguage={this.state.selectedLanguage}
           onSelect={this.updateLanguage}
         />
-        {!this.state.repos ? <Loading text='DOWNLOADING'/> : <RepoGrid repos={this.state.repos} />}
+        {!this.state.repos ? <Loading /> : <RepoGrid repos={this.state.repos} />}
       </div>
     )
   }
--- app/components/loading.js
+++ app/components/loading.js
@@ -16,6 +16,32 @@ class Loading extends React.Component {
       text: props.text
     };
   }
+
+  componentDidMount() {
+
+    var stopper = this.props.text + '...';
+
+    this.interval = window.setInterval(function () {
+      if (this.state.text === stopper) {
+        this.setState(function () {
+          return {
+            text: this.props.text
+          }
+        })
+      } else {
+        this.setState(function (prevState) {
+          return {
+            text: prevState.text + '.'
+          }
+        });
+      }
+    }.bind(this), this.props.speed)
+  }
+
+  componentWillUnmount () { 
+    window.clearInterval(this.interval);
+  }
+
   render() {
     return (
       <p style={styles.content}>
@@ -27,10 +53,12 @@ class Loading extends React.Component {
 
 Loading.propTypes = {
   text: PropTypes.string.isRequired,
-}
+  speed: PropTypes.number.isRequired,
+};
 
 Loading.defaultProps = {
-  text: 'Loading'
+  text: 'Loading',
+  speed: 300
 };
 
 module.exports = Loading;
